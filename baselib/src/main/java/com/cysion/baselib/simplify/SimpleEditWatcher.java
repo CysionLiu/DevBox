aa655493101e8f1473880be604236e67e96c547e
liuxshang <liuxshang@163.com>
2018-10-11 17:31:44 +0800

    加入两个简化接口

---
+++ baselib/src/main/java/com/cysion/baselib/simplify/SimpleEditWatcher.java
@@ -0,0 +1,24 @@
+package com.cysion.baselib.simplify;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * 简化接口调用
+ */
+public class SimpleEditWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
